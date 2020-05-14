2ef5c7a6a31976d789746f6acfb6fef12fb4d657
Neo Pan <panlw@kinco.cn>
2020-05-14 17:45:07 +0800

    initial commit

---
+++ src/test/java/xyz/neopan/getting/started/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package xyz.neopan.getting.started;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
